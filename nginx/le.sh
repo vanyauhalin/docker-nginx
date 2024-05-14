def2784cb47e9accc195f9a42f9e1e1a07e0cc08
vanyauhalin <vanyauhalin@gmail.com>
2024-05-15 00:31:56 +0400

    replace 15min with weekly for the cron job

--- nginx/le.sh
+++ nginx/le.sh
@@ -77,7 +77,7 @@ job() {
 	file=$(readlink -f "$0")
 	dir=$(dirname "$file")
 	sh="#!/bin/sh\nLE_EMAIL=\"$LE_EMAIL\" LE_DOMAINS=\"$LE_DOMAINS\" \"$file\" renew >> \"$dir/le.log\" 2>&1\n"
-	file=/etc/periodic/15min/le
+	file=/etc/periodic/weekly/le
 	printf "%b" "$sh" > $file
 	chmod +x $file
 }
