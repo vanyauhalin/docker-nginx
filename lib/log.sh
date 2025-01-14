8984d778385708ce47d793ee081ba73fdd13977c
vanyauhalin <vanyauhalin@gmail.com>
2025-01-15 00:48:27 +0400

    use the nmeter command to obtain nanoseconds for the log message

--- lib/log.sh
+++ lib/log.sh
@@ -8,7 +8,8 @@ set -ue
 log() {
 	r="$1"
 
-	d=$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")
+	# https://unix.stackexchange.com/questions/167968/date-in-milliseconds-on-openwrt-on-arduino-yun/#answer-362748
+	d=$(date -u "+%FT$(nmeter -d0 "%3t" | head -n1)Z")
 	l=$(echo "$r" | cut -d" " -f1)
 	m=$(echo "$r" | cut -d" " -f2-)
 
